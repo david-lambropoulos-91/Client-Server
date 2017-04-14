8e0473fd2060f54d0339580cc60519b964959772
David Lambropoulos <d.a.lambropoulos91@gmail.com>
2017-04-13 23:54:51 -0400

    Server and client can successfully establish a connection over a port specified by the user

--- server.c
+++ server.c
@@ -6,7 +6,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd>
+#include <unistd.h>
 #include <errno.h>
 #include <string.h>
 #include <netdb.h>
@@ -47,7 +47,7 @@ int claim_port( const char* port )
 	}
 	else if( errno = 0, ( sd = socket( result->ai_family, result->ai_socktype, result->ai_protocol ) ) == -1 )
 	{
-		write( 1, message, sprintf( message, "\x1b[1;31msocket() failed. In file %s in function %s at line %d.\x1b[0m\n"), __FILE__, func, __LINE__ );
+		write( 1, message, sprintf( message, "\x1b[1;31msocket() failed. In file %s in function %s at line %d.\x1b[0m\n", __FILE__, func, __LINE__ ) );
 		freeaddrinfo( result );
 		return -1;
 	}
@@ -67,6 +67,110 @@ int claim_port( const char* port )
 	}
 	else
 	{
-		write
+		write( 1, message, sprintf( message, "\x1b[1;32mSUCCESS : Bind to port %s\x1b[0m\n", port ) );
+		freeaddrinfo( result );
+		return sd;	// bind() succeeded
 	}
+}
+
+
+static char *ps( unsigned int x, char* s, char* p )
+{
+	return x == 1 ? s : p;
+}
+
+void periodic_action_handler( int signo, siginfo_t* ignore, void* ignore2 )
+{
+	if( signo == SIGALRM )
+	{
+		sem_post( &actionCycleSemaphore );
+	}
+}
+
+void* periodic_action_cycle_thread( void* ignore )
+{
+
+	return 0;
+}
+
+void* client_session_thread( void* args )
+{
+	int sd;
+	char request[ RCVBUFSIZE ];
+	char response[ RCVBUFSIZE ];
+
+	return 0;
+}
+
+int main(int argc, char** argv)
+{
+	int sd;
+	char message[ RCVBUFSIZE ];
+	char temp[ RCVBUFSIZE ];
+	pthread_t tid;
+	pthread_attr_t kernel_attr;
+	socklen_t ic;
+	int fd;
+	struct sockaddr_in senderAddr;
+	int*	fdptr;
+	char* func = "main";
+
+	if(argc < 2)
+	{
+		sprintf( temp, "\x1b[1;31mNo port provided!!!!\x1b[0m\n" );
+		ERR_EXIT( temp );
+	}
+
+	if( pthread_attr_init( &kernel_attr ) != 0 )
+	{
+		sprintf( temp, "\x1b[1;31mpthread_attr_init() failed in file %s in function %s at line %d.\x1b[0m\n", __FILE__, func, __LINE__ );
+		ERR_EXIT( temp );
+	}
+	else if( pthread_attr_setscope( &kernel_attr, PTHREAD_SCOPE_SYSTEM ) != 0 )
+	{
+		sprintf( temp, "\x1b[1;31mpthread_attr_setscope() failed in file %s in function %s at line %d.\x1b[0m\n", __FILE__, func, __LINE__ );
+		ERR_EXIT( temp );
+	}
+	else if( ( sd = claim_port( argv[ 1 ] ) ) == -1 )
+	{
+		sprintf( temp, "\x1b[1;31mCould not bind to port %s errno %s in file %s in function %s at line %d.\x1b[0m\n", argv[ 1 ], strerror( errno ), __FILE__, func, __LINE__ );
+		ERR_EXIT( temp );
+	}
+	else if( listen( sd, 100 ) == -1 )
+	{
+		sprintf( temp, "\x1b[1;31mlisten() failed in file %s in function %s at line %d.\x1b[0m\n", __FILE__, func, __LINE__ );
+		close( sd );
+		ERR_EXIT( temp );
+	}
+	else
+	{
+		ic = sizeof( senderAddr );
+
+		while( ( fd = accept( sd, ( struct sockaddr * ) &senderAddr, &ic ) ) != -1 )
+		{
+			fdptr = ( int* ) malloc( sizeof( int ) );
+			*fdptr = fd;
+
+			if( pthread_create( &tid, &kernel_attr, client_session_thread, fdptr ) != 0 )
+			{
+				sprintf( temp, "\x1b[1;31mpthread_create() failed in file %s in function %s at line %d.\x1b[0m\n", __FILE__, func, __LINE__ );
+				ERR_EXIT( temp );
+			}
+			else if( pthread_create( &tid, &kernel_attr, periodic_action_cycle_thread, 0 ) != 0 )
+			{
+				sprintf( temp, "\x1b[1;31mpthread_create() failed in file %s in function %s at line %d.\x1b[0m\n", __FILE__, func, __LINE__ );
+				ERR_EXIT( temp );
+			}
+			else
+			{
+				continue;
+			}
+		}
+		close( sd );
+		return 0;
+	}
+	
+
+
+	return 0;
 }
--- server.h
+++ server.h
@@ -1,3 +1,3 @@
 #define MAXPENDING 5	/* Max outstanding connection requests */
 #define RCVBUFSIZE 256	/* Size of receive buffer */
-#define ERR_EXIT(msg) { perror(msg); exit(1) }
+#define ERR_EXIT(msg) { perror(msg); exit(1); }
